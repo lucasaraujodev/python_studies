c023f5258e3472c4d2201ea8cd553ab71c3ab5fb
Lucas <lucasaraujodev.contact@gmail.com>
2024-12-25 17:24:26 -0300

    modules python introduction

---
+++ python-fundamentals/II-Módulos e PIP/1-módulos.py
@@ -0,0 +1,7 @@
+import calc
+from calc import div # importa apenas uma função do módulo
+
+print(calc.sum(10,2))
+print(calc.sub(10,2))
+print(calc.mult(10,2))
+print(div(10,2)) # não precisa invocar o módulo
