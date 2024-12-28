7aa8f11c37621ec81b2e6b208209bbfe76b37ae7
Lucas <lucasaraujodev.contact@gmail.com>
2024-12-28 10:02:47 -0300

    os module

---
+++ python-fundamentals/II-Módulos e PIP/3-os.py
@@ -0,0 +1,19 @@
+import os
+
+# Funcionalidades do módulo os
+help('os')
+
+# Pasta atual
+print(os.getcwd())
+
+# Lista arquivos e pastas
+print(os.listdir())    
+
+# Versão do SO
+os.system('ver')
+
+# Configurações da Máquina
+os.system('systeminfo')
+
+# Limpar a tela
+os.system('cls')
